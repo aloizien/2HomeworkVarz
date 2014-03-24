0bec7f22e46e30865aff1791c89b12889c7b0441
aloizien <aloizien@gmail.com>
2014-03-24 21:01:18 +0000

    p

---
+++ 01.DeclareVariables/DeclareVars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class DeclareVars
+{
+    static void Main()
+    {
+        ushort a52130 = 52130;
+        sbyte am115 = -115;
+        long a4825932 = 4825932;
+        byte a97 = 97;
+        short am10000 = -10000;
+
+        Console.WriteLine(a52130); Console.Write("52130\n");
+        Console.WriteLine("#############################\n");
+        Console.WriteLine(am115);  Console.Write("-115\n");
+        Console.WriteLine("#############################\n");
+        Console.WriteLine(a4825932); Console.Write("4825932\n");
+        Console.WriteLine("#############################\n");
+        Console.WriteLine(a97); Console.Write("97\n");
+        Console.WriteLine("#############################\n");
+        Console.WriteLine(am10000); Console.Write("-10000\n");
+
+
+
+    }
+} 
